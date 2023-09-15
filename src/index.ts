c021bfd50d4b8d4719d7d05483ecdf4b11bbef25
Juan David <juancho04reyes@gmail.com>
2023-09-15 18:54:50 -0500

    Final version super optimizada

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import data from "../data"
+import data from "./data"
 import "./componets/divPixel/divPixel"
 
 class AppContainer extends HTMLElement{
@@ -17,44 +17,20 @@ class AppContainer extends HTMLElement{
         link.setAttribute("href", "/dist/index.css")
         this.shadowRoot?.appendChild(link)
 
-        this.createSections(data)
-    }
-
-    createSections(data: any) {
-        const sectionList = []
-
-        for (let i = 0; i < 9; i++) {
+        data.images.map( (bigImage)=> {
             const section = this.ownerDocument.createElement("section")
-            section.classList.add(`section${i}`)
-            sectionList.push(section)
+            section.classList.add("bigImage")
             this.shadowRoot?.appendChild(section)
-        }
-
-        this.createPixels(data.images[0], sectionList[0])
-        this.createPixels(data.images[1], sectionList[1])
-        this.createPixels(data.images[2], sectionList[2])
-        this.createPixels(data.images[3], sectionList[3])
-        this.createPixels(data.images[4], sectionList[4])
-        this.createPixels(data.images[5], sectionList[5])
-        this.createPixels(data.images[6], sectionList[6])
-        this.createPixels(data.images[7], sectionList[7])
-    }
-
-    createPixels(data: any, section: any) {
-        const imageArray: Array<number> = []
-        data.map((list: Array<number>) => {
-                list.map( (pixel)=> {
-                imageArray.push(pixel)
+            bigImage.map( (pixelLine) => {
+                section.style.width = `${pixelLine.length}0px`
+                pixelLine.map( (pixel) => {
+                    const pixelPrueba = this.ownerDocument.createElement("div-pixel")
+                    pixelPrueba.setAttribute("color", `${pixel}`)
+                    section.appendChild(pixelPrueba)
+                })
             })
         })
-        
-        imageArray.map( (pixel: any) => {
-            const div = this.ownerDocument.createElement("div-pixel")
-            div.setAttribute("color", pixel)
-            section.appendChild(div)
-        })
     }
-
 }
 
 customElements.define("app-container", AppContainer)
